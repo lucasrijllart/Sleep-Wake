774585f47656b7a2c88e94112601e74c18dc84b2
Lucas Rijllart <airsha@live.fr>
2017-06-28 15:06:22 +0100

    Sensor and Motor values now displayed correctly
    * Simulator.py
    Added calcuations for the positions of the sensors and motors
    
    - OldVehicle.py
    Removed obsolete file

--- SimulatorPackage/Simulator.py
+++ SimulatorPackage/Simulator.py
@@ -1,30 +1,56 @@
 import sys
 import pygame
+import math
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 from SimulatorPackage.Vehicle import Vehicle
 from SimulatorPackage.Light import Light
 
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Braitenberg vehicle simulation")
+for e in pygame.event.get():
+    pygame.key.set_repeat(50, 20)
+clock = pygame.time.Clock()
+
+white = 240, 240, 240
+background = pygame.Surface(screen.get_size())
+background = background.convert(background)
+background.fill(white)
+
+iterations = 300
+myfont = pygame.font.SysFont('monospace', 12)
+v1 = Vehicle(screen, [100, 100], 180)
+light = Light([400, 400])
+all_sprites = pygame.sprite.RenderPlain(v1, light)
+
+
+def show_sensors_motors(vehicle):
+    bearing = vehicle.bearing[-1]
+    radius = vehicle.R + 5
+
+    direction_x = (vehicle.pos[-1][0] - math.cos(bearing) * radius) + math.sin(bearing) * radius / 2
+    direction_y = (vehicle.pos[-1][1] + math.sin(bearing) * radius) + math.cos(bearing) * radius / 2
+    left_wheel = myfont.render(format(v1.wheel_l, '.1f'), 1, (0, 0, 0))
+    screen.blit(left_wheel, [int(direction_x), int(direction_y)])
+
+    direction_x = (vehicle.pos[-1][0] + math.cos(bearing) * radius) + math.sin(bearing) * radius / 2
+    direction_y = (vehicle.pos[-1][1] - math.sin(bearing) * radius) + math.cos(bearing) * radius / 2
+    right_wheel = myfont.render(format(v1.wheel_r, '.1f'), 1, (0, 0, 0))
+    screen.blit(right_wheel, [int(direction_x), int(direction_y)])
+
+    direction_x = (vehicle.pos[-1][0] - math.cos(bearing) * radius) - math.sin(bearing) * radius
+    direction_y = (vehicle.pos[-1][1] + math.sin(bearing) * radius) - math.cos(bearing) * radius
+    left_sensor = myfont.render(format(v1.sensors[-1][0], '.2f'), 1, (100, 100, 0))
+    screen.blit(left_sensor, [int(direction_x), int(direction_y)])
+
+    direction_x = (vehicle.pos[-1][0] + math.cos(bearing) * radius) - math.sin(bearing) * radius
+    direction_y = (vehicle.pos[-1][1] - math.sin(bearing) * radius) - math.cos(bearing) * radius
+    right_sensor = myfont.render(format(v1.sensors[-1][1], '.2f'), 1, (100, 100, 0))
+    screen.blit(right_sensor, [int(direction_x), int(direction_y)])
+
 
 def run():
-
-    pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Braitenberg vehicle simulation")
-    for e in pygame.event.get():
-        pygame.key.set_repeat(50, 20)
-    clock = pygame.time.Clock()
-
-    white = 240, 240, 240
-    background = pygame.Surface(screen.get_size())
-    background = background.convert(background)
-    background.fill(white)
-
-    iterations = 300
-    myfont = pygame.font.SysFont('monospace', 12)
-    v1 = Vehicle(screen, [100, 100], 180)
-    light = Light([400, 400])
-    all_sprites = pygame.sprite.RenderPlain(v1, light)
 
     for t in range(1, iterations):
         clock.tick()
@@ -45,21 +71,13 @@ def run():
 
         all_sprites.draw(screen)
 
-        left_wheel = myfont.render(format(v1.wheel_l, '.1f'), 1, (0, 0, 0))
-        screen.blit(left_wheel, (v1.rect.center[0] - v1.rect.width / 2, v1.rect.center[1]))
-        right_wheel = myfont.render(format(v1.wheel_r, '.1f'), 1, (0, 0, 0))
-        screen.blit(right_wheel, (v1.rect.center[0] + v1.rect.width / 2, v1.rect.center[1]))
-        left_sensor = myfont.render(format(v1.sensors[-1][0], '.2f'), 1, (100, 100, 0))
-        screen.blit(left_sensor, (v1.rect.center[0] - v1.rect.width / 2, v1.rect.center[1] - v1.rect.height / 2))
-        right_sensor = myfont.render(format(v1.sensors[-1][1], '.2f'), 1, (100, 100, 0))
-        screen.blit(right_sensor, (v1.rect.center[0] + v1.rect.width / 2, v1.rect.center[1] - v1.rect.height / 2))
+        show_sensors_motors(v1)
 
         pygame.display.flip()
         pygame.display.set_caption('Braitenberg vehicle simulation - ' + str(format(clock.get_fps(), '.0f')) + 'fps')
 
     pygame.display.quit()
     print('Finished:')
-    print(v1.sensors[0])
 
     i = range(1, iterations)
     left_sensor = [value[0] for value in v1.sensors]
