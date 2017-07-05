8a55843acec699f653a5cf6af237e6c3bdc61c61
Lucas Rijllart <airsha@live.fr>
2017-07-05 18:26:47 +0100

    Modified GA to be a class
    * GA
    Created a class that initialises a GA object with the possibility of displaying graphics at the end
    Renamed all the methods that shouldn't be accessed to have an underscore before the name
    Created run() and run_random() to run the GA with a vehicle and light or making those random
    
    run() takes 3 mandatory arguments: veh_pos, veh_angle, light_pos
    example: run([100, 120], 40, [50, 50])
    
    run() and run_random() have 4 optional parameters: individuals, generations, crossover_rate and
    mutation_rate

--- SimulatorPackage/GA.py
+++ SimulatorPackage/GA.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.pyplot as plt
 
 
-def init_pool():
+def _init_pool(individuals):
     pool = []
     for i in range(0, individuals):
         ind = [random.randint(0, 20) for x in range(0, 6)]
@@ -14,151 +14,160 @@ def init_pool():
     return pool
 
 
-def mutate(ind):
+def _mutate(ind, mutation_rate):
     for i in range(0, len(ind)):
         if random.random() < mutation_rate:
             ind[i] = random.randint(0, 20)
     return ind
 
 
-def perform_crossover(indwin, indlos, indwinfit):
-    for i in range(0, len(indwin)):
-        if random.random() < crossover_rate:
-            indlos[i] = indwin[i]
-    # mutate until fitter or counter
-    counter = 0
-    while indwinfit > get_fitness(indlos) and counter > 2000:
-        indlos = mutate(indlos)
-        counter += 1
-        print(counter)
-    return [indwin, indlos]
+class GA:
 
+    def __init__(self, graphics=False):
+        self.graphics = graphics
 
-def get_all_fitnesses(pool):
-    i = 0
-    fitnesses = []
-    for individual in pool:
-        fitness = get_fitness(individual)
-        fitnesses.append([i, individual, fitness])
-        i += 1
-    return fitnesses
+        self.individual_reached_light = [False]
+        self.sim = Simulator()
 
+        # init values as None, as they will be rewritten in run or run_random
+        self.start_x, self.start_y, self.start_a = None, None, None
+        self.light = None
+        self.iterations = None
 
-def get_fitness(ind):
-    # create sprites
-    vehicle = Attacker([start_x, start_y], start_a)
-    vehicle.set_values(ind[0], ind[1], ind[2], ind[3], ind[4], ind[5])
+    def _perform_crossover(self, indwin, indlos, indwinfit, crossover_rate, mutation_rate):
+        for i in range(0, len(indwin)):
+            if random.random() < crossover_rate:
+                indlos[i] = indwin[i]
+        # mutate until fitter or counter
+        counter = 0
+        while indwinfit > self._get_fitness(indlos) and counter > 2000:
+            indlos = _mutate(indlos, mutation_rate)
+            counter += 1
+            print(counter)
+        return [indwin, indlos]
 
-    # create Simulation
-    vehicle = sim.run_simulation(iterations, False, vehicle, light)
+    def _get_all_fitnesses(self, pool):
+        i = 0
+        fitnesses = []
+        for individual in pool:
+            fitness = self._get_fitness(individual)
+            fitnesses.append([i, individual, fitness])
+            i += 1
+        return fitnesses
 
-    if vehicle.reached_light:
-        individual_reached_light.append(True)
-        winner.append(ind)
+    def _get_fitness(self, ind):
+        # create sprites
+        vehicle = Attacker([self.start_x, self.start_y], self.start_a)
+        vehicle.set_values(ind[0], ind[1], ind[2], ind[3], ind[4], ind[5])
 
-    # calculate fitness with 1/distance
-    distance = 0
-    for step in vehicle.pos:
-        distance += math.sqrt((step[0] - light_x) ** 2 + (step[1] - light_y) ** 2)
-    fitness = 1 / distance
+        # create Simulation
+        vehicle = self.sim.run_simulation(self.iterations, False, vehicle, self.light)
 
-    '''
-    # calculate fitness with average distance
-    distances = []
-    for step in vehicle.pos:
-        distances.append(math.sqrt((step[0] - light_x) ** 2 + (step[1] - light_y) ** 2))
-    fitness = 0
-    for distance in distances:
-        fitness += distance
-    fitness = fitness / len(distances)
-    fitness = 1 / fitness
-    '''
+        if vehicle.reached_light:
+            self.individual_reached_light.append(True)
 
-    return fitness*100000
+        # calculate fitness with 1/distance
+        distance = 0
+        for step in vehicle.pos:
+            distance += math.sqrt((step[0] - self.light.pos[0]) ** 2 + (step[1] - self.light.pos[1]) ** 2)
+        fitness = 1 / distance
 
+        '''
+        # calculate fitness with average distance
+        distances = []
+        for step in vehicle.pos:
+            distances.append(math.sqrt((step[0] - light_x) ** 2 + (step[1] - light_y) ** 2))
+        fitness = 0
+        for distance in distances:
+            fitness += distance
+        fitness = fitness / len(distances)
+        fitness = 1 / fitness
+        '''
 
-def tournament(individual1, individual2):
-    fitness1 = get_fitness(individual1)
-    fitness2 = get_fitness(individual2)
+        return fitness*100000
 
-    if fitness1 >= fitness2:
-        return perform_crossover(individual1, individual2, fitness1)
-    else:
-        ind2, ind1 = perform_crossover(individual2, individual1, fitness2)
-        return [ind1, ind2]
+    def _tournament(self, individual1, individual2, crossover_rate, mutation_rate):
+        fitness1 = self._get_fitness(individual1)
+        fitness2 = self._get_fitness(individual2)
 
-
-def run_winner(graphics, ind):
-    print 'Running: ' + str(ind) + str(get_fitness(ind[1]))
-    ind = ind[1]
-    vehicle = Attacker([start_x, start_y], start_a)
-    vehicle.set_values(ind[0], ind[1], ind[2], ind[3], ind[4], ind[5])
-    # create Simulation
-    sim.run_simulation(iterations, graphics, vehicle, light)
-
-
-def run_ga(graphics):
-    pool = init_pool()
-    all_fitness = get_all_fitnesses(pool)
-    print(all_fitness)
-    best_ind = [0, 0, 0]
-    for ind in all_fitness:
-        if ind[2] > best_ind[2]:
-            best_ind = ind
-    print 'best: ' + str(best_ind)
-    best_fit = [best_ind[2]]
-    for generation in range(0, individuals * generations):
-        print '\rgen: ' + str(generation) + '/' + str(individuals*generations),
-
-        # find 2 random individuals
-        rand_ind1 = random.randint(0, individuals-1)
-        rand_ind2 = random.randint(0, individuals-1)
-        if rand_ind1 == rand_ind2:
-            rand_ind2 = random.randint(0, individuals-1)
-        # compare fitnesses
-        ind1, ind2 = tournament(pool[rand_ind1], pool[rand_ind2])
-
-        # winner overwrites loser with crossover
-        pool[rand_ind1] = ind1
-        pool[rand_ind2] = ind2
-        all_fitness[rand_ind1][2] = get_fitness(ind1)
-        all_fitness[rand_ind2][2] = get_fitness(ind2)
-        if all_fitness[rand_ind1][2] > best_ind[2]:
-            best_ind = all_fitness[rand_ind1]
-            best_fit.append(all_fitness[rand_ind1][2])
-        elif all_fitness[rand_ind2][2] > best_ind[2]:
-            best_ind = all_fitness[rand_ind2]
-            best_fit.append(all_fitness[rand_ind2][2])
+        if fitness1 >= fitness2:
+            return self._perform_crossover(individual1, individual2, fitness1, crossover_rate, mutation_rate)
         else:
-            best_fit.append(best_ind[2])
+            ind2, ind1 = self._perform_crossover(individual2, individual1, fitness2, crossover_rate, mutation_rate)
+            return [ind1, ind2]
 
-    print '\nBest fitness: ' + str(best_fit[-1]) + str(best_ind)
-    print 'Reached light: ' + str(individual_reached_light[-1])
-    run_winner(graphics, best_ind)
-    plt.plot(range(0, len(best_fit)), best_fit)
-    plt.show()
+    def _run_winner(self, graphics, ind):
+        print 'Running: ' + str(ind) + str(self._get_fitness(ind[1]))
+        ind = ind[1]
+        vehicle = Attacker([self.start_x, self.start_y], self.start_a)
+        vehicle.set_values(ind[0], ind[1], ind[2], ind[3], ind[4], ind[5])
+        # create Simulation
+        self.sim.run_simulation(self.iterations, graphics, vehicle, self.light)
 
+    def _start_ga(self, individuals, generations, crossover_rate, mutation_rate):
+        pool = _init_pool(individuals)
+        all_fitness = self._get_all_fitnesses(pool)
+        print(all_fitness)
+        best_ind = [0, 0, 0]
+        for ind in all_fitness:
+            if ind[2] > best_ind[2]:
+                best_ind = ind
+        print 'best: ' + str(best_ind)
+        best_fit = [best_ind[2]]
+        for generation in range(0, individuals * generations):
+            print '\rgen: ' + str(generation) + '/' + str(individuals * generations),
 
-individuals = 20
-generations = 4
-crossover_rate = 0.7
-mutation_rate = 0.3
-individual_reached_light = [False]
-winner = []
+            # find 2 random individuals
+            rand_ind1 = random.randint(0, individuals - 1)
+            rand_ind2 = random.randint(0, individuals - 1)
+            if rand_ind1 == rand_ind2:
+                rand_ind2 = random.randint(0, individuals - 1)
+            # compare fitnesses
+            ind1, ind2 = self._tournament(pool[rand_ind1], pool[rand_ind2], crossover_rate, mutation_rate)
 
-sim = Simulator()
+            # winner overwrites loser with crossover
+            pool[rand_ind1] = ind1
+            pool[rand_ind2] = ind2
+            all_fitness[rand_ind1][2] = self._get_fitness(ind1)
+            all_fitness[rand_ind2][2] = self._get_fitness(ind2)
+            if all_fitness[rand_ind1][2] > best_ind[2]:
+                best_ind = all_fitness[rand_ind1]
+                best_fit.append(all_fitness[rand_ind1][2])
+            elif all_fitness[rand_ind2][2] > best_ind[2]:
+                best_ind = all_fitness[rand_ind2]
+                best_fit.append(all_fitness[rand_ind2][2])
+            else:
+                best_fit.append(best_ind[2])
 
-# create random values
-start_x = random.randint(25, sim.window_width - 25)
-start_y = random.randint(25, sim.window_height - 25)
-start_a = random.randint(0, 360)
-light_x = random.randint(25, sim.window_width - 25)
-light_y = random.randint(25, sim.window_height - 25)
-light = Light([light_x, light_y])
+        print '\nBest fitness: ' + str(best_fit[-1]) + str(best_ind)
+        print 'Reached light: ' + str(self.individual_reached_light[-1])
+        self._run_winner(self.graphics, best_ind)
+        plt.plot(range(0, len(best_fit)), best_fit)
+        plt.show()
 
-graphics = True
-start_light_dist = math.sqrt((light_x - start_x)**2 + (light_y - start_y)**2)
-print start_light_dist
-iterations = int(start_light_dist/2)  # Halved number to limit number of frames and tested with a big distance
+    def run_random(self, individuals=20, generations=5, crossover_rate=0.7, mutation_rate=0.3):
+        # create random values
+        self.start_x = random.randint(25, self.sim.window_width - 25)
+        self.start_y = random.randint(25, self.sim.window_height - 25)
+        self.start_a = random.randint(0, 360)
+        light_x = random.randint(25, self.sim.window_width - 25)
+        light_y = random.randint(25, self.sim.window_height - 25)
+        self.light = Light([light_x, light_y])
 
-run_ga(graphics)
+        start_light_dist = math.sqrt((light_x - self.start_x) ** 2 + (light_y - self.start_y) ** 2)
+        print 'vehicle available frames: ' + str(start_light_dist)
+        self.iterations = int(start_light_dist / 2)  # Halved number to limit num of frames and tested with a big dist
+
+        self._start_ga(individuals, generations, crossover_rate, mutation_rate)
+
+    def run(self, veh_pos, veh_angle, light_pos, individuals=20, generations=5, crossover_rate=0.7, mutation_rate=0.3):
+        self.start_x = veh_pos[0]
+        self.start_y = veh_pos[1]
+        self.start_a = veh_angle
+        self.light = Light(light_pos)
+
+        start_light_dist = math.sqrt((light_pos[0] - self.start_x) ** 2 + (light_pos[1] - self.start_y) ** 2)
+        print 'vehicle available frames: ' + str(start_light_dist)
+        self.iterations = int(start_light_dist / 2)  # Halved number to limit num of frames and tested with a big dist
+
+        self._start_ga(individuals, generations, crossover_rate, mutation_rate)
