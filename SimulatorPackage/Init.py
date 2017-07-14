06b7a97310e710dcf08e4db0692195fdb5511115
lucasrijllart <airsha@live.fr>
2017-07-14 12:29:32 +0100

    Put narx networks in folder called narx

--- SimulatorPackage/Init.py
+++ SimulatorPackage/Init.py
@@ -5,20 +5,23 @@ from Cycle import Cycle
 
 cycle = Cycle()
 
-learning_runs = 10
+learning_runs = 30
 learning_time = 1000
-testing_time = 1
-input_delay = 30
-output_delay = 30
-max_epochs = 25
+testing_time = 100
+input_delay = 100
+output_delay = 100
+max_epochs = 100
+
+random_movement = 100
+look_ahead = 50
 
 # cycle.wake_learning(vehicle_runs=learning_runs, vehicle_iter=learning_time, test_iter=testing_time, input_delay=input_delay,
 #                     output_delay=output_delay, net_max_iter=max_epochs, show_graph=False)
 
-cycle.wake_learning(random_movements=100, train_network=False, learning_runs=learning_runs,
-                    learning_time=learning_time, input_delay=input_delay, output_delay=output_delay,
-                    max_epochs=max_epochs)
+cycle.wake_learning(random_movement, train_network=False, learning_runs=learning_runs, learning_time=learning_time,
+                    testing_time=testing_time, input_delay=input_delay, output_delay=output_delay,
+                    max_epochs=max_epochs, show_graph=False)
 
-cycle.sleep(net_filename='delay100[10, 10]', look_ahead=100, generations=50)
+cycle.sleep(net_filename='r100t1000d25-25', look_ahead=look_ahead, individuals=30, generations=20)
 
-cycle.wake_testing()
+cycle.wake_testing(iterations=look_ahead)
